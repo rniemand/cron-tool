edf0a0348913ee6add86356207ca60bcaa39089d
Richard Niemand <niemand.richard@gmail.com>
2021-10-26 16:17:59 -0600

    Stubbing logging

--- src/CronTool/Program.cs
+++ src/CronTool/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using Rn.NetCore.Common.Logging;
 
 namespace CronTool
 {
   internal class Program
   {
+    private static IServiceProvider _serviceProvider;
+
     static void Main(string[] args)
     {
+      ConfigureDI();
+
+      _serviceProvider
+        .GetRequiredService<ILoggerAdapter<Program>>()
+        .Debug("Hello World");
+
       Console.WriteLine("Hello World!");
+    }
+
+    private static void ConfigureDI()
+    {
+      var services = new ServiceCollection();
+
+      var config = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", true, true)
+        .Build();
+
+      services
+        // Configuration
+        .AddSingleton<IConfiguration>(config)
+        
+        // Logging
+        .AddSingleton(typeof(ILoggerAdapter<>), typeof(LoggerAdapter<>))
+        .AddLogging(loggingBuilder =>
+        {
+          // configure Logging with NLog
+          loggingBuilder.ClearProviders();
+          loggingBuilder.SetMinimumLevel(LogLevel.Trace);
+          loggingBuilder.AddNLog(config);
+        });
+
+      _serviceProvider = services.BuildServiceProvider();
     }
   }
 }
