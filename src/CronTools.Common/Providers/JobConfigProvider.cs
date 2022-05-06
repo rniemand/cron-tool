95be5d5d9e2284f6f51aa9024127ced98e02ca7d
Richard Niemand <niemand.richard@gmail.com>
2022-05-06 14:36:44 -0600

    Basic JobConfigProvider

--- src/CronTools.Common/Providers/JobConfigProvider.cs
+++ src/CronTools.Common/Providers/JobConfigProvider.cs
@@ -7,6 +7,8 @@ using CronTools.Common.Models;
 using Microsoft.Extensions.DependencyInjection;
 using Rn.NetCore.Common.Abstractions;
 using Rn.NetCore.Common.Extensions;
+using Rn.NetCore.Common.Helpers;
+using Rn.NetCore.Common.Logging;
 
 namespace CronTools.Common.Providers;
 
@@ -18,71 +20,65 @@ public interface IJobConfigProvider
 public class JobConfigProvider : IJobConfigProvider
 {
   private readonly CronToolConfig _config;
+  private readonly ILoggerAdapter<JobConfigProvider> _logger;
   private readonly IDirectoryAbstraction _directory;
+  private readonly IFileAbstraction _file;
   private readonly IPathAbstraction _path;
+  private readonly IJsonHelper _jsonHelper;
   private readonly List<string> _jobNames;
 
   public JobConfigProvider(IServiceProvider serviceProvider)
   {
-    _config = serviceProvider.GetRequiredService<IConfigProvider>().GetConfig();
+    _logger = serviceProvider.GetRequiredService<ILoggerAdapter<JobConfigProvider>>();
     _directory = serviceProvider.GetRequiredService<IDirectoryAbstraction>();
     _path = serviceProvider.GetRequiredService<IPathAbstraction>();
+    _file = serviceProvider.GetRequiredService<IFileAbstraction>();
+    _jsonHelper = serviceProvider.GetRequiredService<IJsonHelper>();
+    _config = serviceProvider.GetRequiredService<IConfigProvider>().GetConfig();
 
     _jobNames = DiscoverJobs();
   }
 
+
   public JobConfig Resolve(string jobName)
   {
     // TODO: [JobConfigProvider.Resolve] (TESTS) Add tests
+    if (string.IsNullOrWhiteSpace(jobName))
+      return null;
 
-    Console.WriteLine();
+    // Check to see if we have discovered the requested job
+    if (!JobExists(jobName))
+    {
+      _logger.LogWarning("No job with name '{name}' found in: {path}", jobName, _config.JobsDirectory);
+      return null;
+    }
 
+    // Ensure that the job file still exists
+    var jobFilePath = _path.Join(_config.JobsDirectory, $"{jobName}.json");
+    if (!_file.Exists(jobFilePath))
+    {
+      _logger.LogWarning("Unable to find job file: {path}", jobFilePath);
+      return null;
+    }
 
-    //if (string.IsNullOrWhiteSpace(jobName))
-    //  return null;
+    // Read contents of the config file
+    var rawJson = _file.ReadAllText(jobFilePath);
+    var jobConfig = _jsonHelper.DeserializeObject<JobConfig>(rawJson);
 
-    //// Check to see if we have discovered the requested job
-    //var jobKey = jobName.LowerTrim();
-    //if (!_jobFiles.ContainsKey(jobKey))
-    //{
-    //  _logger.LogWarning("Requested job {key} was not found in {directory}",
-    //    jobKey,
-    //    _config.JobsDirectory
-    //  );
+    // Handle disabled jobs
+    if (!jobConfig.Enabled)
+    {
+      _logger.LogWarning("Requested job {name} is disabled", jobConfig.Name);
+      return null;
+    }
 
-    //  return null;
-    //}
+    _logger.LogInformation("Loaded config for {name} ({path})",
+      jobConfig.Name,
+      jobFilePath);
 
-    //// Ensure that the job file still exists
-    //var jobFilePath = _jobFiles[jobKey];
-    //if (!_file.Exists(jobFilePath))
-    //{
-    //  _logger.LogWarning("Job file {path} no longer exists", jobFilePath);
-    //  _jobFiles.Remove(jobKey);
-    //  return null;
-    //}
-
-    //// Read contents of the config file
-    //var rawJson = _file.ReadAllText(jobFilePath);
-    //var jobConfig = _jsonHelper.DeserializeObject<JobConfig>(rawJson);
-
-    //// Handle disabled jobs
-    //if (!jobConfig.Enabled)
-    //{
-    //  _logger.LogWarning("Requested job {name} is disabled", jobConfig.Name);
-    //  return null;
-    //}
-
-    //_logger.LogInformation("Loaded config for {name} ({path})",
-    //  jobConfig.Name,
-    //  jobFilePath
-    //);
-
-    //return jobConfig;
-
-
-    return null;
+    return jobConfig;
   }
+
 
   private void EnsureDirectoryExists(string path)
   {
@@ -106,4 +102,8 @@ public class JobConfigProvider : IJobConfigProvider
       .Select(jsonFile => _path.GetFileNameWithoutExtension(jsonFile))
       .ToList();
   }
+
+  private bool JobExists(string jobName) =>
+    // TODO: [JobConfigProvider.JobExists] (TESTS) Add tests
+    _jobNames.Any(x => x.IgnoreCaseEquals(jobName));
 }
