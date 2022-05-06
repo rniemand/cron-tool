c95d30e60cd08df6817bba25f48238b9b66a48f7
Richard Niemand <niemand.richard@gmail.com>
2022-05-06 16:32:12 -0600

    Working in command line args

--- src/CronTool/Program.cs
+++ src/CronTool/Program.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using CronTools.Common.Extensions;
 using CronTools.Common.Services;
@@ -7,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NLog.Extensions.Logging;
+using System.CommandLine;
 
 namespace CronTool;
 
@@ -16,11 +19,7 @@ internal class Program
 
   static async Task Main(string[] args)
   {
-    ConfigureDI();
-
-    await _serviceProvider
-      .GetRequiredService<ICronRunnerService>()
-      .RunAsync(args);
+    await BuildCommand().InvokeAsync(args);
   }
 
   private static void ConfigureDI()
@@ -44,5 +43,38 @@ internal class Program
       });
 
     _serviceProvider = services.BuildServiceProvider();
+  }
+
+  private static async Task Bob(string jobName, string rootDir)
+  {
+    ConfigureDI();
+
+    await _serviceProvider
+      .GetRequiredService<ICronRunnerService>()
+      .RunAsync(new string[] {jobName});
+  }
+
+  private static RootCommand BuildCommand()
+  {
+    var rootDir = new Option<string>(
+      "--rootDir",
+      getDefaultValue: () => Environment.CurrentDirectory,
+      description: "Root directory to use when resolving jobs");
+    rootDir.AddAlias("-r");
+
+    var jobName = new Option<string>(
+      "--job",
+      getDefaultValue: () => string.Empty,
+      description: "The name of the job you wish to run");
+    jobName.AddAlias("-j");
+
+    var rootCommand = new RootCommand {rootDir, jobName};
+    rootCommand.Description = "Rn.CronTool";
+
+    rootCommand.SetHandler(
+      async (string name, string dir) => await Bob(name, dir),
+      jobName, rootDir);
+
+    return rootCommand;
   }
 }
