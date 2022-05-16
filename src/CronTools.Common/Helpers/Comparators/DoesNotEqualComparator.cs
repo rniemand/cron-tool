c7cc71531c4bfb76b6cc05be525fc503adfb9d55
Richard Niemand <niemand.richard@gmail.com>
2022-05-16 16:56:45 -0600

    Added base DoesNotEqualComparatorTests

--- src/CronTools.Common/Helpers/Comparators/DoesNotEqualComparator.cs
+++ src/CronTools.Common/Helpers/Comparators/DoesNotEqualComparator.cs
@@ -16,8 +16,7 @@ public class DoesNotEqualComparator : IComparator
 
   public bool Compare(object source, string target)
   {
-    // TODO: [DoesNotEqualComparator.Compare] (TESTS) Add tests
-    _logger.LogDebug("Running condition: '{source}' != '{target}'", source, target);
+    _logger.LogDebug("Comparing: '{source}' != '{target}'", source, target);
 
     if (source is long longValue)
       return longValue != CastHelper.StringToLong(target);
