48764b5c8a5f9c79f6865aadf5168ec232f3a7ae
Richard Niemand <niemand.richard@gmail.com>
2022-05-25 17:00:12 -0600

    Added initial metric support

--- src/CronTools.Common/Services/JobRunnerService.cs
+++ src/CronTools.Common/Services/JobRunnerService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using CronTools.Common.Exceptions;
 using CronTools.Common.Factories;
@@ -7,6 +8,8 @@ using CronTools.Common.Providers;
 using CronTools.Common.Resolvers;
 using CronTools.Common.Utils;
 using Rn.NetCore.Common.Logging;
+using Rn.NetCore.Metrics;
+using Rn.NetCore.Metrics.Builders;
 
 namespace CronTools.Common.Services;
 
@@ -25,6 +28,7 @@ public class JobRunnerService : IJobRunnerService
   private readonly IJobUtils _jobUtils;
   private readonly IConditionHelper _conditionHelper;
   private readonly IJobConfigProvider _jobConfigProvider;
+  private readonly IMetricService _metrics;
 
   public JobRunnerService(
     ILoggerAdapter<JobRunnerService> logger,
@@ -32,7 +36,8 @@ public class JobRunnerService : IJobRunnerService
     IJobActionResolver actionResolver,
     IJobUtils jobUtils,
     IConditionHelper conditionHelper,
-    IJobConfigProvider jobConfigProvider)
+    IJobConfigProvider jobConfigProvider,
+    IMetricService metrics)
   {
     _logger = logger;
     _jobFactory = jobFactory;
@@ -40,65 +45,84 @@ public class JobRunnerService : IJobRunnerService
     _jobUtils = jobUtils;
     _conditionHelper = conditionHelper;
     _jobConfigProvider = jobConfigProvider;
+    _metrics = metrics;
   }
 
   public async Task RunJobAsync(JobConfig jobConfig)
   {
+    var builder = CreateMetricBuilder(nameof(RunJobAsync))
+      .WithCustomTag1(jobConfig.Name)
+      .WithCustomInt1(jobConfig.Steps.Count);
+
     var continueRunningSteps = true;
     var stepNumber = 1;
     var jobContext = _jobFactory.CreateRunningJobContext(jobConfig);
     var argumentResolver = _jobFactory.GetJobArgumentResolver();
     var jobName = jobConfig.Name;
 
-    foreach (var step in jobConfig.Steps)
+    try
     {
-      if (!continueRunningSteps)
-        continue;
-
-      var resolvedAction = _actionResolver.Resolve(step);
-      if (resolvedAction is null)
-        throw new MissingActionException(step.Action);
-
-      // Ensure that the current job step arguments are valid
-      var stepContext = _jobFactory.CreateRunningStepContext(jobContext, step, stepNumber++);
-      if (!_jobUtils.ValidateStepArgs(resolvedAction, stepContext))
+      using (builder.WithTiming())
       {
-        if (!step.QuitOnFailure)
-          continue;
-
-        _logger.LogError("Job '{job}' step '{step}' has invalid arguments, stopping", jobName, step.StepId);
-        break;
-      }
-
-      // Handle step conditions, quit if we need to
-      if (!_conditionHelper.CanRunJobStep(jobContext, stepContext))
-      {
-        // Can we continue?
-        if (!(step.Condition?.StopOnFailure ?? false))
+        foreach (var step in jobConfig.Steps)
         {
-          _logger.LogInformation("Skipping job step: {name}", step.Name);
-          continue;
-        }
+          if (!continueRunningSteps)
+            continue;
 
-        // We can't continue, stop this job.
-        _logger.LogWarning("Job '{job}' step '{step}' conditions failed, stopping job.", jobName, step.StepId);
-        break;
+          var resolvedAction = _actionResolver.Resolve(step);
+          if (resolvedAction is null)
+            throw new MissingActionException(step.Action);
+
+          // Ensure that the current job step arguments are valid
+          var stepContext = _jobFactory.CreateRunningStepContext(jobContext, step, stepNumber++);
+          if (!_jobUtils.ValidateStepArgs(resolvedAction, stepContext))
+          {
+            if (!step.QuitOnFailure)
+              continue;
+
+            _logger.LogError("Job '{job}' step '{step}' has invalid arguments, stopping", jobName, step.StepId);
+            break;
+          }
+
+          // Handle step conditions, quit if we need to
+          if (!_conditionHelper.CanRunJobStep(jobContext, stepContext))
+          {
+            // Can we continue?
+            if (!(step.Condition?.StopOnFailure ?? false))
+            {
+              _logger.LogInformation("Skipping job step: {name}", step.Name);
+              continue;
+            }
+
+            // We can't continue, stop this job.
+            _logger.LogWarning("Job '{job}' step '{step}' conditions failed, stopping job.", jobName, step.StepId);
+            break;
+          }
+
+          // Handle a successful step
+          var outcome = await resolvedAction.ExecuteAsync(jobContext, stepContext, argumentResolver);
+          if (outcome.Succeeded)
+            continue;
+
+          if (!step.QuitOnFailure)
+            continue;
+
+          // Step failed, can't continue on error, log and stop
+          _logger.LogError("Step failed, stopping job");
+          continueRunningSteps = false;
+        }
       }
 
-      // Handle a successful step
-      var outcome = await resolvedAction.ExecuteAsync(jobContext, stepContext, argumentResolver);
-      if (outcome.Succeeded)
-        continue;
-
-      if (!step.QuitOnFailure)
-        continue;
-
-      // Step failed, can't continue on error, log and stop
-      _logger.LogError("Step failed, stopping job");
-      continueRunningSteps = false;
+      _logger.LogInformation("Job '{name}' ({id}) executed", jobName, jobConfig.JobId);
     }
-
-    _logger.LogInformation("Job '{name}' ({id}) executed", jobName, jobConfig.JobId);
+    catch (Exception ex)
+    {
+      builder.WithException(ex);
+    }
+    finally
+    {
+      await _metrics.SubmitBuilderAsync(builder);
+    }
   }
 
   public async Task RunJobAsync(string jobFileName)
@@ -127,4 +151,7 @@ public class JobRunnerService : IJobRunnerService
       await RunJobAsync(jobName);
     }
   }
+
+  private ServiceMetricBuilder CreateMetricBuilder(string method) =>
+    new ServiceMetricBuilder(nameof(JobRunnerService), method);
 }
